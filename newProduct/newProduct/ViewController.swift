f61ba03a0bb71a06bd963835c0c5e500dec5e8d5
LiXueKui <269673806@qq.com>
2016-10-18 08:17:42 +0800

    201610180817

---
+++ newProduct/newProduct/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  newProduct
+//
+//  Created by qianfeng on 16/10/18.
+//  Copyright © 2016年 LiXueKui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("客户端创建的路径")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
